a5ad8011ef7c82b5b4bba8f50393e5254ccf3299
ManliestBen <benjamintmanley@gmail.com>
2019-10-29 21:58:03 -0500

    add server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var favicon = require('serve-favicon');
+
+require('./config/database');
+
+var todosRouter = require('./routes/api/todos');
+
+var app = express();
+
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use('/api/todos', todosRouter);
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+    console.log(`Express app listening on port ${port}`);
+});
